618caefe886d516f2d8e95d9d952d7edff31ec29
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-08-27 13:46:11 +1000

    Seeing if it means what it said about git add

---
+++ scripts/test.js
@@ -0,0 +1,7 @@
+function testFunction() {
+  // eslint-disable-next-line no-console
+  console.log('This is a bad function');
+}
+window.addEventListener('DOMContentLoaded', () => {
+  testFunction();
+});
