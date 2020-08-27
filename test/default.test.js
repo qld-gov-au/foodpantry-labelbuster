a01f94de6072a0205fae5bb8dc808a1d6537920d
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-08-27 10:54:50 +1000

    Added in karma and jasmine

---
+++ test/default.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+describe('Testing the testing harness has been setup correctly', function () {
+  it('true should be true', function () {
+    expect(true).toBe(true);
+  });
+
+  it('document should exist', function () {
+    expect(document).toBeDefined();
+  });
+});
