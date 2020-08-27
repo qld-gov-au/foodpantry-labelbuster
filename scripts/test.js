234b9fb109c6cf2e42bce85abf4e61af19e71969
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-08-27 13:47:29 +1000

    See if this works

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,7 @@ function testFunction() {
   // eslint-disable-next-line no-console
   console.log('This is a bad function');
 }
+
 window.addEventListener('DOMContentLoaded', () => {
   testFunction();
 });
