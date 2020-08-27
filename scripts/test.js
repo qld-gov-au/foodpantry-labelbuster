6dfb54a45e09fa5acb5f015c0b98a1608882b781
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-08-27 15:04:52 +1000

    Removed useless cruft

--- scripts/test.js
+++
@@ -1,8 +0,0 @@
-function testFunction() {
-  // eslint-disable-next-line no-console
-  console.log('This is a bad function');
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  testFunction();
-});
