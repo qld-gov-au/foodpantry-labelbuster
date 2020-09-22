84fc3c8f9130a9a757adcacef13839030833f32b
Daniel Turnerx <001.daniel.turner@gmail.com>
2020-09-22 16:31:47 +1000

    Added in lit-html and started the process of cleaning up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { LabelBuster } from './components/label-buster';
-import { SectionNavigation } from './components/section-navigation';
+// import { SectionNavigation } from './components/section-navigation';
 import { ButtonGroup } from './components/button-group';
 import attachStepHandler from './scripts/step-handlers';
 
@@ -16,19 +16,19 @@ import attachStepHandler from './scripts/step-handlers';
       pageHeader.removeChild(document.querySelector('h1'));
     }
 
-    let sectionNav = document.querySelector(
-      '#qg-section-nav > ul > li:nth-child(1)'
-    );
-    if (!sectionNav) {
-      sectionNav = document.querySelector('#formnav');
-      sectionNav.display = 'block';
-    }
+    // let sectionNav = document.querySelector(
+    //   '#qg-section-nav > ul > li:nth-child(1)',
+    // );
+    // if (!sectionNav) {
+    //   sectionNav = document.querySelector('#formnav');
+    //   sectionNav.display = 'block';
+    // }
 
-    const unorderdList = document.createElement('ul');
-    unorderdList.classList = 'lb';
-    sectionNav.appendChild(unorderdList);
-    const sectionNavTarget = sectionNav.querySelector('ul');
+    // const unorderdList = document.createElement('ul');
+    // unorderdList.classList = 'lb';
+    // sectionNav.appendChild(unorderdList);
+    // const sectionNavTarget = sectionNav.querySelector('ul');
 
-    const sectionNavigation = new SectionNavigation(sectionNavTarget);
+    // const sectionNavigation = new SectionNavigation(sectionNavTarget);
   });
 })();
