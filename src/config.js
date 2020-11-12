631f7d11bc3fc324878fab2efa54a274c1eaa375
Jordan Binskin <jordan.binskin@gmail.com>
2020-11-12 15:22:43 +1100

    auto email

--- src/config.js
+++ src/config.js
@@ -46,5 +46,7 @@ export const configuration = {
     type: localStorage,
     name: 'lbcompleted',
   },
-  extraTriggersOnActions: {},
+  extraTriggersOnActions: {
+    next: 'checkForAutoEmail',
+  },
 };
