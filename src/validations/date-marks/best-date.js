f52b62d05dbce04f02613a272a45508c117187b8
Daniel Turnerx <001.daniel.turner@gmail.com>
2021-01-19 10:46:45 +1000

    Fixed up the date field to allow forward slashes

--- src/validations/date-marks/best-date.js
+++ src/validations/date-marks/best-date.js
@@ -5,7 +5,7 @@
     valid = "The date mark must 20 characters or less.";
     return;
   }
-  if (/[|&;$%"<>()\{\}+\\\/]/.test(input)) {
+  if (/[|&;$%"<>()\{\}+\\]/.test(input)) {
     valid = 'Please do not include html or special characters e.g. not any of these |&;$%"<>(){}+\/';
     return;
   }
