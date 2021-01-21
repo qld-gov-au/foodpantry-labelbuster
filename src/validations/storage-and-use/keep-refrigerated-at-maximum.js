14f85ffc7c80f0459d46f2861eaf0b1e0b0d9d54
Nishtha <nishtha.cfa@smartservice.qld.gov.au>
2021-01-21 13:17:11 +1100

    Partial changes as of now - C4A-477

--- src/validations/storage-and-use/keep-refrigerated-at-maximum.js
+++ src/validations/storage-and-use/keep-refrigerated-at-maximum.js
@@ -5,22 +5,16 @@
   if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt && typeof data.storageConditions.keepRefrigeratedAtMaximum === 'undefined') {
     valid = "The keep refrigerated to temperature must be entered when the food is to be refrigerated between a specified temperature range.";
     return;
-  } // Maximum can`t be greater than or equal to the minimum.
+  }
 
-
-  if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt && data.storageConditions.keepRefrigeratedAtMinimum >= data.storageConditions.keepRefrigeratedAtMaximum) {
+  // Maximum can`t be greater than or equal to the minimum.
+  if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt &&  data.storageConditions.keepRefrigeratedAtMinimum && data.storageConditions.keepRefrigeratedAtMaximum && data.storageConditions.keepRefrigeratedAtMinimum >= data.storageConditions.keepRefrigeratedAtMaximum) {
     valid = "The keep refrigerated to temperature must higher than the keep refrigerated from temperature when the food is to be refrigerated between a specified temperature range.";
     return;
-  } // Maximum can`t be zero or below
+  }
 
-
-  if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt && data.storageConditions.keepRefrigeratedAtMaxmium <= 0) {
-    valid = "The keep refrigerated from temperature must be a number between 0 \xB0C and 30 \xB0C when the food is to be refrigerated between a specified temperature range.";
-    return;
-  } // Maximum can`t be 30 or above.
-
-
-  if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt && data.storageConditions.keepRefrigeratedAtMaximum >= 30) {
+  // Maximum can`t be zero or below / 30 or above.
+  if (data && data.directionsForUse && data.storageConditions.keepRefrigeratedAt && (data.storageConditions.keepRefrigeratedAtMaxmium <= 0 || data.storageConditions.keepRefrigeratedAtMaximum >= 30)) {
     valid = "The keep refrigerated from temperature must be a number between 0 \xB0C and 30 \xB0C when the food is to be refrigerated between a specified temperature range.";
     return;
   }
