ebc838243a2960f06a333f4a061ffdfc8cadbc07
Nishtha <nishtha.cfa@smartservice.qld.gov.au>
2021-01-21 15:38:19 +1100

    storage other condition changed - regex

--- src/validations/storage-and-use/storage-other-details.js
+++ src/validations/storage-and-use/storage-other-details.js
@@ -13,5 +13,11 @@
     characters.`;
     return;
   }
+
+  if (/[|&;$%"<>()\{\}+\\\/]/.test(input)) {
+    valid = 'Please do not include html or special characters e.g. not any of these |&;$%"<>(){}+\/';
+    return;
+  }
+
   valid = true;
 })();
