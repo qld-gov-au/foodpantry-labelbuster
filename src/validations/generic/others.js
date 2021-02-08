b4010668f9a1df1171df8a4944cd5da7e35c3a39
Daniel Turnerx <001.daniel.turner@gmail.com>
2021-02-08 12:43:34 +1000

    Added a generic validation for all the other fields that won't have any

---
+++ src/validations/generic/others.js
@@ -0,0 +1,8 @@
+(function () {
+  if (/[|&;$%"<>()\{\}+\\\/]/.test(input)) {
+    valid = 'Please do not include html or special characters e.g. not any of these |&;$%"<>(){}+\/';
+    return;
+  }
+
+  valid = true;
+})();
