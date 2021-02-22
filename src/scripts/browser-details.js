f3edc493be126cb5b97ce17de9dba9f537501f43
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-02-22 11:35:43 +1000

    updated spacing

--- src/scripts/browser-details.js
+++ src/scripts/browser-details.js
@@ -1,19 +1,18 @@
 export default function browserDetails() {
-    const ua = navigator.userAgent;
-    let tem;
-    let matchBrowser = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    const navigatorUserAgent = navigator.userAgent;
+    let predictVersion;
+    let matchBrowser = navigatorUserAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
     if (/trident/i.test(matchBrowser[1])){
-      tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-      return {name:'IE',version:(tem[1]||'')};
+      predictVersion = /\brv[ :]+(\d+)/g.exec(navigatorUserAgent) || [];
+      return {name:'IE',version:(predictVersion[1]||'')};
     }
     if (matchBrowser[1] === 'Chrome'){
-      tem = ua.match(/\bOPR|Edge\/(\d+)/)
-      if(tem!=null)   {return {name:'Edge', version:tem[1]};}
+      predictVersion = navigatorUserAgent.match(/\bOPR|Edge\/(\d+)/)
+      if(predictVersion != null)   { return { name:'Edge', version:predictVersion[1]}; }
     }
-
     matchBrowser = matchBrowser[2]? [matchBrowser[1], matchBrowser[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    tem = ua.match(/version\/(\d+)/i)
-    if (tem != null) { matchBrowser.splice(1,1,tem[1]); }
+    predictVersion = navigatorUserAgent.match(/version\/(\d+)/i)
+    if (predictVersion != null) { matchBrowser.splice(1,1,predictVersion[1]); }
     return {
       name: matchBrowser[0],
       version: matchBrowser[1]
