1cfbe290a4c0d0e87edce6dc40567b6d80bbeb14
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-03-04 09:14:41 +1000

    C4A-513 removed date validation as requested

--- src/validations/date-marks/best-date.js
+++ src/validations/date-marks/best-date.js
@@ -5,9 +5,5 @@
     valid = "The date mark must 20 characters or less.";
     return;
   }
-  if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(input)) {
-    valid = 'Please use the format dd/mm/yyyy';
-    return;
-  }
   valid = true;
 })();
