bda3d8a239c366803329eaadc5e2afaec66587b0
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-03-04 11:18:23 +1000

    C4A-513 date validation

--- src/validations/date-marks/best-date.js
+++ src/validations/date-marks/best-date.js
@@ -5,5 +5,10 @@
     valid = "The date mark must 20 characters or less.";
     return;
   }
+  // check if not a valid character
+  if (!/^[a-zA-Z0-9" |/.,']*$/.test(input)) {
+    valid = 'You have entered an invalid character, please use only letters, numbers and these special characters "|./,\'';
+    return;
+  }
   valid = true;
 })();
