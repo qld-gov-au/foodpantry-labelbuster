cca1837481cab8e3bfea5e481974517bf6fd8b11
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-03-04 11:32:47 +1000

    C4A-513 added fullstop as a valid

--- src/validations/statements/salt.js
+++ src/validations/statements/salt.js
@@ -10,8 +10,8 @@
     return;
   }
   // check if not a valid character
-  if (!/^[a-zA-Z0-9 "%/,'°]*$/.test(input)) {
-    valid = 'You have entered an invalid character, please use only letters, numbers and these special characters "°%,\'';
+  if (!/^[a-zA-Z0-9 ".%/,'°]*$/.test(input)) {
+    valid = 'You have entered an invalid character, please use only letters, numbers and these special characters ".°%,\'';
     return;
   }
   valid = true;
