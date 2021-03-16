c197d15cdc1afb054da40a49fbf14c810549a8f7
Daniel Turnerx <001.daniel.turner@gmail.com>
2021-03-16 15:17:39 +1000

    Adding the deleted code back into this repo too

--- src/components/formio-wrapper.js
+++ src/components/formio-wrapper.js
@@ -437,6 +437,12 @@ export class FormioWrapper {
         cancelButton.displayed = false;
         nextButton.title = this.config.buttons.overwriteValue;
       }
+
+      if (this.config.buttons.overwriteFirstButtonShowExtras) {
+        cancelButton.displayed = true;
+        previousButton.displayed = true;
+        previousButton.event = 'formiowrapperCancel';
+      }
     }
 
     if (page === this.wizard.pages.length - 1) {
