372718fc0a7e657be6886d97369d32675b9387d7
Ross Webster <ross.webster@smartservice.qld.gov.au>
2021-05-25 17:11:20 +1000

    [C4A-648] Fixing a similar instance

--- src/components/partials/help-guide-lb-ingredient.js
+++ src/components/partials/help-guide-lb-ingredient.js
@@ -1664,15 +1664,14 @@ export default () => html`
         <div class="collapsing-section" id="id-panel-content-1">
           <p>
             <b
-              >If the food sold in a small package where the package has a
+              >If the food is sold in a small package where the package has a
               surface area of less than 100 cm<sup>2</sup>:</b
             >
             <br />
             The ingredient list is not required is not required for food in
             small packages.
             <br />
-            For example: A small a packet of chewing gum or a small bar of
-            chocolate.
+            For example: A small bar of chocolate.
           </p>
           <section>
             <h4>Further reading</h4>
