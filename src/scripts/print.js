a4145a99c9eec35daada3eadc7230c778e1d7b6c
Alex Oommen <alex.oommen@lighthousehq.com.au>
2021-06-16 12:49:27 +0530

    fixed collapsed accordion when clicked on print

--- src/scripts/print.js
+++ src/scripts/print.js
@@ -1,14 +1,10 @@
-export function printScreen(e, sourceSelector, accordianSelector = null) {
+export function printScreen(e, sourceSelector) {
   document.querySelector('.btn-print').remove();
   document.querySelector('.help-guide-close').remove();
-  if (accordianSelector) {
-    const expands = document.querySelectorAll(accordianSelector);
-    expands.forEach(btn => btn.click());
-  }
-  setTimeout(() => {
-    const contentSource = document.getElementById(sourceSelector);
-    document.body.innerHTML = contentSource.innerHTML;
-    window.print();
-    window.location.reload();
-  }, 2000);
+  const contentSource = document.getElementById(sourceSelector);
+  const checkboxes = contentSource.querySelectorAll("input[type='checkbox']");
+  checkboxes.forEach(checkbox => checkbox.setAttribute('checked', true));
+  document.body.innerHTML = contentSource.innerHTML;
+  window.print();
+  window.location.reload();
 }
